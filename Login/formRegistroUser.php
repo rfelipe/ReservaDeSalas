12b91ef39da880b0303717394e8634143918a6ca
rfelipe <f.questao@gmail.com>
2019-05-09 21:27:24 -0300

    faz login e Seçao

---
+++ Login/formRegistroUser.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head> <include >
+</head>
+	<body>
+		<h1>Registro de Usuario</h1>
+			<form method='POST' action='formRegistrarSave.php' enctype='text/plain'>
+			
+
+				<label> Nome:
+					<input type="texto" name="nome">
+				</label>
+			<p>
+				<label> Senha:
+					<input type="text" name="senha">
+				</label>
+
+			<input type="submit" value="Salvar" action="submit"> 
+			
+			</form>
+	</body>
+</hml>
